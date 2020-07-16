4dcf9a889b22e24c7fd05b13fc7bf3f374e1c8ff
Joel Antonio Rodriguez <j rodriguez@kdomain.com>
2020-07-16 15:19:00 -0400

    Rebotar Objeto en la pantalla

---
+++ Objectos En Ejes/Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    float STARTINGFORCE = 500F;
+    bool hasStarted = false;
+
+    Rigidbody rbody;
+    Vector3 startingForce = new Vector3(1, 1); 
+
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody>();   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted && Input.GetButtonDown("Fire1")) 
+        {
+            hasStarted = true;
+            startingForce *= STARTINGFORCE;
+
+            rbody.AddForce(startingForce);
+        }
+        
+    }
+}
