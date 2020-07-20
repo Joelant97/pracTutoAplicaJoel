e420d1f0f5a974f9db79b58543c31ee90de99499
Joel Antonio Rodriguez <j rodriguez@kdomain.com>
2020-07-20 11:53:38 -0400

    Caida Libre

---
+++ Objectos En Ejes/Assets/Scripts/SphereBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehaviour : MonoBehaviour
+{
+    Vector3 deltaPos = new Vector3();
+    Vector3 currentSpeed = new Vector3();
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Posicion final --> Yf = Y0 +V0y * T + (aT^2)/2
+        deltaPos.y = currentSpeed.y * Time.deltaTime + Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2) / 2;
+        gameObject.transform.Translate(deltaPos);
+
+        currentSpeed += Physics.gravity * Time.deltaTime;
+
+    }
+}
